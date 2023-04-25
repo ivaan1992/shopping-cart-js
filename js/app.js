ce9dce6d320ad12579c424a68799dce70d00172e
Ivan Gaona <ivaan.lg.92@gmail.com>
2023-04-25 13:14:34 -0600

    Now when you add a new course,you can see the image, name, price and quantity

--- js/app.js
+++ js/app.js
@@ -48,11 +48,19 @@ function mostrarProductos() {
 
     // Recorrer carrito y generar el html
     articulosCarrito.forEach( curso => {
+        const { imagen, titulo, precio, cantidad, id } = curso
         const row = document.createElement('tr');
         row.innerHTML = 
         `
             <td>
-                ${curso.titulo}
+                <img src ='${imagen}' width='100'>
+                
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href='#' class='borrar-curso' data-id='${id}'>X</a>
             </td>
         `;
 
